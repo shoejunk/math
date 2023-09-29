dae1e45fdae47786fc095e559fbb019d81545cfa
Joseph Shunk <joseph.shunk@gmail.com>
2023-09-28 23:44:27 -0700

    Update style etc.

---
+++ source/math.ixx
@@ -0,0 +1,103 @@
+export module stk.math;
+
+#pragma warning(push)
+#pragma warning(disable: 5050) // _M_FP_PRECISE is defined in current command line and not in module command line
+import std.core;
+#pragma warning(pop)
+
+export namespace stk
+{
+	// Returns the next prime number after x. Returns 0 if there is no prime number after uX that fits in a uint32_t.
+	constexpr uint32_t next_prime(uint32_t x)
+	{
+		if (x < 2)
+			return 2;
+
+		if (x == 2)
+			return 3;
+
+		if (x >= 4294960003) // Largest prime number that fits in a uint32_t.
+			return 0;
+
+		uint32_t prime = (x + 1) | 1;
+		while (true)
+		{
+			bool is_prime = true;
+			for (uint32_t divisor = 3; divisor * divisor <= prime; divisor += 2)
+			{
+				if (prime % divisor == 0)
+				{
+					is_prime = false;
+					break;
+				}
+			}
+
+			if (is_prime)
+				return prime;
+
+			prime += 2;
+		}
+	}
+	static_assert(next_prime(0) == 2);
+	static_assert(next_prime(1) == 2);
+	static_assert(next_prime(2) == 3);
+	static_assert(next_prime(3) == 5);
+	static_assert(next_prime(4) == 5);
+	static_assert(next_prime(5) == 7);
+	static_assert(next_prime(6) == 7);
+	static_assert(next_prime(2147483646) == 2147483647);
+	static_assert(next_prime(2147483647) == 2147483659);
+	static_assert(next_prime(4294960002) == 4294960003);
+	static_assert(next_prime(4294960003) == 0);
+	static_assert(next_prime(4294967295) == 0);
+
+	// constexpr and deterministic random number generator.
+	template<class T>
+	constexpr T det_rand_int(T seed, T max)
+	{
+		static_assert(std::is_integral_v<T>, "T must be an integral type");
+		if (max == 0)
+		{
+			return 0;
+		}
+		return (seed * 1664525 + 1013904223) % max;
+	}
+	static_assert(det_rand_int<uint32_t>(0, 1) == 0);
+	static_assert(det_rand_int<uint64_t>(0, 10000) == 4223);
+	static_assert(det_rand_int<uint32_t>(1, 10000) == 8748);
+
+	// Uses the device to generate a non-deterministic random number
+	class rand
+	{
+	public:
+		rand() : m_engine(m_device()) {}
+
+		template<class T>
+		T rand_int(T min, T max)
+		{
+			static_assert(std::is_integral_v<T>, "T must be an integral type");
+			if (min > max)
+			{
+				std::swap(min, max);
+			}
+			std::uniform_int_distribution<T> dist(min, max);
+			return dist(m_engine);
+		}
+
+		template<class T>
+		T rand_float(T min, T max)
+		{
+			static_assert(std::is_floating_point_v<T>, "T must be a floating-point type");
+			if (min > max)
+			{
+				std::swap(min, max);
+			}
+			std::uniform_real_distribution<T> dist(min, max);
+			return dist(m_engine);
+		}
+
+	private:
+		std::random_device m_device;
+		std::mt19937 m_engine;
+	};
+}
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,9 +1,13 @@
 #include <assert.h>
 
 import stk.math;
-import std.core;
 
-using namespace NStk;
+#pragma warning(push)
+#pragma warning(disable: 5050) // _M_FP_PRECISE is defined in current command line and not in module command line
+import std.core;
+#pragma warning(pop)
+
+using namespace stk;
 
 int main(int argc, char* argv[])
 {
@@ -11,39 +15,39 @@ int main(int argc, char* argv[])
 	{
 		if (strcmp(argv[1], "NextPrime") == 0)
 		{
-			int iN = atoi(argv[2]);
-			auto uPrime = NStk::NMath::NextPrime((uint32_t)iN);
-			if (uPrime == 0)
+			int n = atoi(argv[2]);
+			auto prime = next_prime((uint32_t)n);
+			if (prime == 0)
 			{
 				std::cout << "Failure. The next prime is larger than would fit in a uint32_t so I cannot compute it." << std::endl;
 			}
 			else
 			{
-				std::cout << "The next prime number that comes after " << iN << " is " << uPrime << std::endl;
+				std::cout << "The next prime number that comes after " << n << " is " << prime << std::endl;
 			}
 		}
 		else if (argc > 3 && (strcmp(argv[1], "Random") == 0))
 		{
-			auto iSeed = atoi(argv[2]);
-			auto iMax = atoi(argv[3]);
-			auto uR = NStk::NMath::Random(static_cast<uint32_t>(iSeed), static_cast<uint32_t>(iMax));
-			std::cout << uR << std::endl;
+			auto seed = atoi(argv[2]);
+			auto max = atoi(argv[3]);
+			auto r = det_rand_int<uint32_t>(static_cast<uint32_t>(seed), static_cast<uint32_t>(max));
+			std::cout << r << std::endl;
 		}
 		else if (argc > 3 && (strcmp(argv[1], "RandomInt") == 0))
 		{
-			auto iMin = atoi(argv[2]);
-			auto iMax = atoi(argv[3]);
-			NMath::CRandom oRandom;
-			auto iR = oRandom.RandomInt(iMin, iMax);
-			std::cout << iR << std::endl;
+			auto min = atoi(argv[2]);
+			auto max = atoi(argv[3]);
+			stk::rand r;
+			auto r_int = r.rand_int(min, max);
+			std::cout << r_int << std::endl;
 		}
-		else if (argc > 3 && (strcmp(argv[1], "RandomReal") == 0))
+		else if (argc > 3 && (strcmp(argv[1], "RandomFloat") == 0))
 		{
-			auto fMin = atof(argv[2]);
-			auto fMax = atof(argv[3]);
-			NMath::CRandom oRandom;
-			auto fR = oRandom.RandomReal(fMin, fMax);
-			std::cout << fR << std::endl;
+			auto min = atof(argv[2]);
+			auto max = atof(argv[3]);
+			stk::rand r;
+			auto r_float = r.rand_float(min, max);
+			std::cout << r_float << std::endl;
 		}
 	}
 	else
@@ -51,23 +55,13 @@ int main(int argc, char* argv[])
 		std::cout << "Enter \"NextPrime <X>\" to find the next prime that comes after X." << std::endl;
 		std::cout << "Enter \"Random <Seed> <Max>\" to find a random number with the given Seed in the range [0, Max)." << std::endl;
 		std::cout << "Enter \"RandomInt <Min> <Max>\" to find a random int in the range [Min, Max]." << std::endl;
-		std::cout << "Enter \"RandomReal <Min> <Max>\" to find a random number in the range from Min to Max." << std::endl;
+		std::cout << "Enter \"RandomFloat <Min> <Max>\" to find a random number in the range from Min to Max." << std::endl;
 	}
 
-	int32_t aiTest[] = { 5, 4, 4, 8, 2, -1, 0, 3, 10, 9, 7, 6, 1, 2, 3, 4, 5, 6, 7, 8 };
-	auto iSize = sizeof(aiTest) / sizeof(int32_t);
-	int32_t aiTestSorted[] = { 10, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 4, 3, 3, 2, 2, 1, 0, -1 };
-	auto iSizeSorted = sizeof(aiTestSorted) / sizeof(int32_t);
-
-	NMath::InsertionSortDecreasing(aiTest, iSize);
-
-	assert(NMath::Match(aiTestSorted, iSizeSorted, aiTest, iSize));
-
-	assert(NMath::AddBits(std::bitset<1>{ 0 }, std::bitset<1>{ 0 }) == std::bitset<2>{ 0 });
-	assert(NMath::AddBits(std::bitset<1>{ 0 }, std::bitset<1>{ 1 }) == std::bitset<2>{ 1 });
-	assert(NMath::AddBits(std::bitset<1>{ 1 }, std::bitset<1>{ 0 }) == std::bitset<2>{ 1 });
-	auto bitsRes = NMath::AddBits(std::bitset<2>("11"), std::bitset<2>("11"));
-	assert(bitsRes == std::bitset<3>("110"));
+	int32_t test[] = { 5, 4, 4, 8, 2, -1, 0, 3, 10, 9, 7, 6, 1, 2, 3, 4, 5, 6, 7, 8 };
+	auto size = sizeof(test) / sizeof(int32_t);
+	int32_t test_sorted[] = { 10, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 4, 3, 3, 2, 2, 1, 0, -1 };
+	auto size_sorted = sizeof(test_sorted) / sizeof(int32_t);
 
 	return 0;
 }
